3dd1f500c9c7b1b9f52202a39f0f410e5ac516c6
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2017-12-16 16:10:47 -0500

    Update 2darray.js

--- 2darray.js
+++ 2darray.js
@@ -4,7 +4,7 @@
     } else if (typeof module === 'object' && module.exports) {
         module.exports = factory();
     } else {
-        root[tdarrayindex] = factory();
+        root["tdarrayindex"] = factory();
     }
 }(typeof self !== 'undefined' ? self : this, function() {
     var tdarrayindex = {
